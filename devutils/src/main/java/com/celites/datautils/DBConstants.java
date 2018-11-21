8bf8015354d7b200d371fb3b4d92f20bd0796a4d
Prasham <PrashamTrivedi@users.noreply.github.com>
2018-11-21 16:24:14 +0530

    Closes #2

--- devutils/src/main/java/com/celites/datautils/DBConstants.java
+++
@@ -1,9 +0,0 @@
-package com.celites.datautils;
-
-/**
- * Created by Prasham on 21-02-2015.
- */
-public class DBConstants {
-	static final String[] ALL_COLUMNS = {"*"};
-	static final String[] ALL_COUNTS = {"COUNT (*)"};
-}
